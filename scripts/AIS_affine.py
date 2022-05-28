eba2bb7a51d9749d7ca22225f42f2dbd59b4dbe7
August <30163079+augeorge@users.noreply.github.com>
2022-05-27 18:07:39 -0700

    updates
    code updates

--- scripts/AIS_affine.py
+++ scripts/AIS_affine.py
@@ -7,7 +7,7 @@ import time
 import pandas as pd
 import multiprocessing as mp
 #import ray.util.multiprocessing as mp
-
+import ray
 
 ### 12d transporter
 
@@ -44,12 +44,14 @@ def calc_grid_point(K,y_obs,m):
     m.k6_f = 10**K[10]
     m.k6_r = (m.k1_f*m.k2_f*m.k3_f*m.k4_f*m.k5_f*m.k6_f)/(m.k1_r*m.k2_r*m.k3_r*m.k4_r*m.k5_r)
     try:
-        D_tmp = m.simulate(0, 10, 250, selections=['time', 'rxn4'])
-        y_tmp = D_tmp['rxn4']
+        # D_tmp = m.simulate(0, 10, 250, selections=['time', 'rxn4'])
+        D_tmp = m.simulate(0, 5, 125, selections=['time', 'rxn4'])
+        y_tmp = D_tmp['rxn4'][1:]  # remove first point
         sigma = 10**K[11]
         log_like_tmp = calc_norm_log_likelihood(y_tmp,sigma,y_obs)
     except:
-        y_tmp = np.zeros(250)
+        # y_tmp = np.zeros(249)  # remove first point
+        y_tmp = np.zeros(124) 
         sigma = 10**K[11]
         log_like_tmp = calc_norm_log_likelihood(y_tmp,sigma,y_obs)
     return log_like_tmp
@@ -146,7 +148,7 @@ def plot_samples(samples, p_info, beta, run_config):
 
     plt.suptitle(f'1D distributions - beta={beta}')
     plt.tight_layout()
-    plt.savefig(f'ais_affine_{n_walkers}w_{n_steps}s_{thresh}t_{seed}r_dist.png') 
+    plt.savefig(f'ais_affine_{init_samples}i_{K_walkers}w_{N_steps}s_{fractional_weight_target}t_{seed}r_dist.png') 
     
 
 def calculate_weights(log_like, beta_old, beta_new):
@@ -177,7 +179,6 @@ def calculate_p_rel(log_like, beta_old, beta_new):
     assert(np.isclose(np.sum(p_rel),1.0))
     return p_rel
     
-    
 
 if __name__ == "__main__":      
     ### model configuration
@@ -204,7 +205,7 @@ if __name__ == "__main__":
                 
 
                 // Events:
-                E1: at (time >= 5): H_out = 1e-7, S_out = 1e-3;
+                E1: at (time >= 2.5): H_out = 1e-7, S_out = 1e-3;
                 
 
                 // Species initializations:
@@ -284,11 +285,14 @@ if __name__ == "__main__":
     m.integrator.absolute_tolerance = 1e-18
     m.integrator.relative_tolerance = 1e-12
     m.H_out = 5e-8
-    D1 = m.simulate(0, 10, 250, selections=['time', 'rxn4'])
-    y_true = D1['rxn4']
+    #D1 = m.simulate(0, 10, 250, selections=['time', 'rxn4'])
+    D1 = m.simulate(0, 5, 125, selections=['time', 'rxn4'])
+    y_true = D1['rxn4'][1:]  # remove first point
 
     noise_stdev_true = 1e-13
-    y_obs = np.genfromtxt("data_grid_test3_1exp.csv")
+    y_obs = np.genfromtxt("data_grid_test3_1exp_v2.csv")
+    # y_obs = y_true + np.random.normal(0, noise_stdev_true, np.size(y_true))
+    # np.savetxt("data_grid_test3_1exp_v2.csv", y_obs)
 
     plt.figure(figsize=(10,10))
     plt.plot(y_obs, 'o', alpha=0.5)
@@ -296,6 +300,8 @@ if __name__ == "__main__":
     plt.ylim(-2.5e-11, 2.5e-11)
     plt.ylabel('y')
     plt.xlabel('t')
+    # plt.savefig('test.png')
+
 
     p_info = [   
         ["log_k1_f",6,12,10],
@@ -312,6 +318,7 @@ if __name__ == "__main__":
         ["log_sigma",np.log10(5e-14), np.log10(5e-13), -13],
     ]
     param_ref = [p_i[3] for p_i in p_info]
+    labels = [p[0] for p in p_info]
     print(param_ref)
 
     # beta_test = [1e-6, 1e-4, 0.001, 0.01, 0.1, 0.25, 0.5, 0.75, 1.0]
@@ -331,21 +338,27 @@ if __name__ == "__main__":
     np.random.seed(seed)
     dim = 12
     max_iter = 1e5
-    fractional_weight_target = 0.1
+    save_at = int(10)  # save data every x steps
 
-    init_samples = int(1e6)
-    N_steps = 100
-    K_walkers = 100
+    fractional_weight_target = 0.1
+    init_samples = int(1e5)
+    N_steps = 10
+    K_walkers = 1000
     NK_total_samples = N_steps*K_walkers
-    burn_in = 1000
+    burn_in = int(0.1*NK_total_samples)
+    init_beta_jump_target = 0.001
+    #init_ESS = K_walkers
+    init_fractional_weight_target = K_walkers/init_samples
 
     M_sub_samples = NK_total_samples - burn_in #10*K_walkers
-    assert((M_sub_samples < NK_total_samples) & (M_sub_samples >= 10*K_walkers))
-
+    assert ((M_sub_samples < NK_total_samples) & (M_sub_samples >= 2*K_walkers)), f" {M_sub_samples} < {NK_total_samples} and {M_sub_samples} >= {10*K_walkers} "
+   
     print(f'random seed: {seed}')
     print(f'n dim: {dim}')
     print(f'max iterations: {max_iter}')
     print(f'init samples: {init_samples}')
+    print(f'init beta jump target: {init_beta_jump_target}')
+    print(f'init fractional weight target: {init_fractional_weight_target}')
     print(f'fractional weight target: {fractional_weight_target}')
     print(f'N steps: {N_steps}')
     print(f'K walkers: {K_walkers}')
@@ -354,6 +367,7 @@ if __name__ == "__main__":
     print(f'burn in: {burn_in}')
     print(f'#####################')
 
+    
 
     ### step 1: initialization
     print('initializing...')
@@ -368,6 +382,7 @@ if __name__ == "__main__":
     # print(f'initial walker shape: {np.shape(samples)}')
     # print(f'p0: {samples[0:2]}')
 
+    t0_2 = time.time()
     pos_list=[]
     for i in range(init_samples):
         p0_list_tmp = set_p0()
@@ -377,15 +392,34 @@ if __name__ == "__main__":
     print(f'initial walker shape: {np.shape(samples)}')
     print(f'p0: {samples[0:2]}')
 
+    print(f"samples.size {samples.size } * samples.itemsize {samples.itemsize} = {samples.size * samples.itemsize}")
+    tf_2 = time.time()
+    print(f"initial samples: {tf_2 - t0_2}")
+
+    # log_like_init = np.ones(init_samples)
+    # print("log_like init")
+    # print(np.shape(log_like_init))
+    
+
 
     print('sampling...')
     iter_i = 0
     while (beta <1) and (iter_i < max_iter):
     
-
-        log_like = np.array([log_prob(theta_i,y_obs,[m,1]) for theta_i in samples])
-        beta_new = calculate_next_beta(log_like, beta, fractional_weight_target)
-        p_rel = calculate_p_rel(log_like, beta, beta_new)
+        if iter_i ==0:
+            t0_3 = time.time()
+            log_like = np.array([log_prob(theta_i,y_obs,[m,1]) for theta_i in samples])
+            beta_new = calculate_next_beta(log_like, beta, init_fractional_weight_target)
+            p_rel = calculate_p_rel(log_like, beta, beta_new)
+            print(f"beta_new={beta_new}")
+            #assert(beta_new>=init_beta_jump_target)
+            tf_3 = time.time()
+            print(f"initial samples: {tf_3 - t0_3}")
+            
+        else:
+            log_like = np.array([log_prob(theta_i,y_obs,[m,1]) for theta_i in samples])
+            beta_new = calculate_next_beta(log_like, beta, fractional_weight_target)
+            p_rel = calculate_p_rel(log_like, beta, beta_new)
         
 
         resamples_index = np.random.choice([ i for i in range(len(samples))],size=K_walkers,p=p_rel)
@@ -405,6 +439,14 @@ if __name__ == "__main__":
         samples = sampler.flatchain[burn_in:]
         print(f'i={iter_i}, new beta={beta_new:.2g}, old beta={beta:.2g}, delta beta={(beta_new-beta):.2g}')
 
+
+        if iter_i%save_at == 0:
+            print('saving current samples and beta...')
+            np.savetxt(f'ais_affine_{init_samples}i_{K_walkers}w_{N_steps}s_{fractional_weight_target}t_{seed}r_data_tmp.csv',
+            samples, delimiter=',', header=",".join(labels), comments=f'# i={iter_i}, beta={beta_new} ')
+        # save to file
+        # save resampled points
+
         iter_i = iter_i +1
         beta=beta_new
         
@@ -418,5 +460,43 @@ if __name__ == "__main__":
 
     plot_samples(samples,p_info, beta_new, [K_walkers, N_steps, fractional_weight_target, seed])
     df = pd.DataFrame(samples, columns=[i[0] for i in p_info])
-    df.to_csv(f'ais_affine_{K_walkers}w_{N_steps}s_{fractional_weight_target}t_{seed}r_data.csv')
+    df.to_csv(f'ais_affine_{init_samples}i_{K_walkers}w_{N_steps}s_{fractional_weight_target}t_{seed}r_data.csv')
 
+# # testing
+#     ray.init() # for single node
+#     # ray.init(address='auto')  # for multiple nodes - e.g. cluster using slurm
+
+#     # generate synthetic data for example
+#     p_true = [-10, -5, 5, 10, 0.2, 0.4, 0.6,]
+#     sigma_true = 2e-3
+#     x_true = np.linspace(-25, 25, 500)
+#     y_true = calc_y(p_true)  # note: you will need to write your own function for your model
+#     y_obs = y_true + np.random.normal(loc=0, scale=sigma_true, size=np.size(y_true))
+
+#     # parameter mesh grid settings
+#     N = 10  # number of resampled points
+#     p_input = [('mu_1', -10, 10, 3),
+#                ('mu_2', -10, 10, 3),
+#                ('mu_3', -10, 10, 3),
+#                ('mu_4', -10, 10, 3),
+#                ('c_1', 0.1, 1, 3),
+#                ('c_2', 0.1, 1, 3),
+#                ('c_3', 0.1, 1, 3),
+#                ]
+#     mg_df = mg.create_grid_coord(p_input, verbose=True)  # calculate parameter mesh grid
+#     mg_id = ray.put(mg_df)  # ray stores the object id of the mesh grid --> don't have to make copies
+
+#     @ray.remote  # used to run ray remote function - this runs in parallel
+#     def f(i, df):
+#         """ This calculates the log likelihood based on the observed data and theta"""
+#         return mg.calc_logl(y_obs, theta=df.iloc[i], func=calc_y)
+
+#     logl_id_list = []
+#     print('calculating log-likelihood...')
+#     for i in range(len(mg_df.index)):
+#         logl_id_list.append( f.remote(i, mg_id))  # list (in the same order as meh grid index)
+#     logl_list = ray.get(logl_id_list)  # run ray remote functions
+
+#     mg_df['logl'] = logl_list  # new df for log-likelihoods
+#     score_df = mg.score_grid(mg_df, verbose=True)  # df relative log-likelihood, likelihood, probability density
+#     start_points, ESS = mg.resample_grid(score_df, N, verbose=True)  # df of start points, effective sample size
