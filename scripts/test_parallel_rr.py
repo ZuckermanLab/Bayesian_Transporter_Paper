b4b7429639f62fb27bc9a0167406dfee9c1cb462
August <30163079+augeorge@users.noreply.github.com>
2022-10-06 12:16:19 -0700

    Update test_parallel_rr.py

--- scripts/test_parallel_rr.py
+++ scripts/test_parallel_rr.py
@@ -1,22 +1,22 @@
 import numpy as np
 import tellurium as te
-import roadrunner as rr
-import multiprocessing as mp
+from pathlib import Path
 import json
-import time
+import ray
+from ray.util import ActorPool
+import matplotlib.pyplot as plt
+
+#import os
+#os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import pocomc as pc 
 
 
-def parse_p_info(p_info, near_global_min=True):
-    '''parse parameter settings data
-    p_info[i] = [parameter name, lower bound, upper bound, reference value]
-    '''
-    p_ref = [p_i[3] for p_i in p_info]
-    p_labels = [p[0] for p in p_info]
-    if near_global_min==True:
-        p_bounds = [(p[3]*0.999, p[3]*1.001) if p[3] > 0 else (p[3]*1.001, p[3]*0.999) for p in p_info]  # near global min
+def calc_norm_log_like_wrapper(mu,sigma,X):
+    # if contains NaN --> p = 0 --> log p = -inf
+    if(np.isnan(mu).any()):
+      return -1e50 #-np.inf
     else:
-        p_bounds = [(p[1], p[2]) for p in p_info]  # default
-    return p_ref, p_labels, np.array(p_bounds)
+      return calc_norm_log_like(mu,sigma,X)
 
 
 def calc_norm_log_like(mu,sigma,X):
@@ -35,14 +35,11 @@ def calc_norm_log_like(mu,sigma,X):
     return log_likelihood
 
 
-#def calc_log_like(K,y_obs,m):
 def calc_log_like(K,y_obs,m):
     '''calculates the log likelihood of a transporter tellurium ODE model m, given data y_obs, and parameters K
     '''
-  
-
+    
     H_out_list = [5e-7]  #  experiments w/ varying external pH conditions [5e-7,0.2e-7]
-    #idx_list = [0,2,4,6,8]  # index of rate pairs used to set attribute, last rate omitted - fix this later 
     k_dict = {
         "1":[0,1],
         "2":[2,3],
@@ -55,8 +52,8 @@ def calc_log_like(K,y_obs,m):
     sigma = 10**K[-1]  # noise standard deviation is last parameter term
 
     for i in range(len(H_out_list)):
-        m.resetAll()
-        #m.resetToOrigin()
+        #m = te.loada(ms)
+        m.resetToOrigin()
         m.H_out = H_out_list[i]  # plot values
         m.integrator.absolute_tolerance = 1e-18
         m.integrator.relative_tolerance = 1e-12
@@ -81,36 +78,189 @@ def calc_log_like(K,y_obs,m):
         y_list.append(y_tmp)
     y_sim = np.hstack(y_list)
     log_like_tmp = calc_norm_log_like(y_sim,sigma,y_obs)
-    
     return log_like_tmp
 
 
+def parse_p_info(p_info, near_global_min=True):
+    '''parse parameter settings data
+    p_info[i] = [parameter name, lower bound, upper bound, reference value]
+    '''
+    p_ref = [p_i[3] for p_i in p_info]
+    p_labels = [p[0] for p in p_info]
+    if near_global_min==True:
+        p_bounds = [(p[3]*0.999, p[3]*1.001) if p[3] > 0 else (p[3]*1.001, p[3]*0.999) for p in p_info]  # near global min
+    else:
+        p_bounds = [(p[1], p[2]) for p in p_info]  # default
+    return p_ref, p_labels, np.array(p_bounds)
+
+
+def get_p0(b_list, n):
+    '''get initial uniform distributed samples using boundaries from b_list, and number of samples n
+    b_list[i][0] = parameter lower bound, b_list[i][1] = parameter upper bound
+    '''
+    p0_array = np.transpose(np.array([np.random.uniform(b[0],b[1],n) for b in b_list]))  # re-arrange array for sampler
+    return p0_array
+
+
+
+def calc_log_prior(p):
+    '''calculates the log of uniform prior distribution
+    if parameter is in range, the probability is log(1)-->0, else log(0)-->-inf
+    '''
+
+    # fix this later - should connect w/ parameter info data file
+    b = 0  # shift parameter priors by 2 (orders of magnitude) to be non-centered
+    lb = np.array([6, -1, -2, -2, 3, -1, -1, 6, -2, -2, -1, 6, -1, 3]) + b  # log10 rate constant prior lower bound + shift
+    ub = np.array([12, 5, 4, 4, 9, 5, 5, 12, 4, 4, 5, 12, 5, 9]) + b  # log10 rate constant prior upper bound + shift
+    sigma_lb = np.log10(5e-14)  # log10 noise sigma prior lower bound 
+    sigma_ub = np.log10(5e-13)  # log10 noise sigma prior upper bound 
+    if ((lb < p[:-1]) & (p[:-1] < ub)).all() and (sigma_lb<p[-1]<sigma_ub):
+        return 0
+    else:
+        return -1e50#-np.inf
+
+
+@ray.remote
+class SimulatorActor(object):
+    """Ray actor to execute simulations."""
+
+    def __init__(self, model_string, y_obs):
+        self.r = te.loada(model_string)
+        self.y_obs = y_obs
+
+
+    def simulate(self, K):
+        """Simulate."""
+        n_pts = 125
+        H_out_list = [5e-7]  #  experiments w/ varying external pH conditions [5e-7,0.2e-7]
+        k_dict = {
+            "1":[0,1],
+            "2":[2,3],
+            "3":[4,5],
+            "4":[6,7],
+            "5":[8,9],
+            "8":[12,13]
+        }
+        y_list = []  # empty list to store flux from different experiments
+        
+
+        for i in range(len(H_out_list)):
+            #m = te.loada(ms)
+            self.r.resetToOrigin()
+            self.r.H_out = H_out_list[i]  # plot values
+            self.r.integrator.absolute_tolerance = 1e-18
+            self.r.integrator.relative_tolerance = 1e-12
+
+            # update tellurium model parameter values (rate constants)
+            for key, value in k_dict.items():
+                setattr(self.r, f'k{key}_f', 10**K[value[0]])
+                setattr(self.r, f'k{key}_r', 10**K[value[1]])
+
+            # k6 and k7 have cycle constraints
+            self.r.k6_f = 10**K[10]
+            self.r.k6_r = (self.r.k1_f*self.r.k2_f*self.r.k3_f*self.r.k4_f*self.r.k5_f*self.r.k6_f)/(self.r.k1_r*self.r.k2_r*self.r.k3_r*self.r.k4_r*self.r.k5_r)
+            self.r.k7_f = 10**K[11]
+            self.r.k7_r = (self.r.k2_f*self.r.k3_f*self.r.k4_f*self.r.k5_f*self.r.k7_f*self.r.k8_f)/(self.r.k2_r*self.r.k3_r*self.r.k4_r*self.r.k5_r*self.r.k8_r)
+
+            try:
+                D_tmp = self.r.simulate(0, 5, n_pts, selections=['time', 'rxn4'])
+                y_tmp = D_tmp['rxn4'][1:]  # remove first point
+            except:
+                y_tmp = np.array(n_pts * [np.NaN]) # return NaN if errors
+            y_list.append(y_tmp)
+        y_sim = np.hstack(y_list)
+        return y_sim
+
+
+    def logl(self, K):
+
+        # print(f"logl got args {args}")
+        # K = args[0]
+
+        y_obs = self.y_obs
+        y_sim = self.simulate(K)
+        sigma = 10**K[-1]  # noise standard deviation is last parameter term
+        return calc_norm_log_like_wrapper(y_sim, sigma, y_obs)
+
+
 if __name__ == "__main__":
+# ray.init(include_dashboard=False, num_cpus=2)
 
-    np.random.seed(0)
-    n_cpus = 6
-    n_sims = 1000
-    model_file = "/Users/georgeau/Desktop/GitHub/Bayesian_Transporter/transporter_model/antiporter_15D_model.txt"
-    obs_data_file = "/Users/georgeau/Desktop/GitHub/Bayesian_Transporter/synthetic_data/synth_data_15D_c1_1expA_125s_v3.csv"
-    parameter_file = "/Users/georgeau/Desktop/GitHub/Bayesian_Transporter/transporter_model/15D_transporter_c1_w_full_priors.json"
+    ### set random seed
+    seed = 0
+    np.random.seed(seed)
+    print(f'using seed: {seed}')
 
+    ### input files
+    dir = Path.cwd()  # set your file path directory
+    model_file = dir / "antiporter_15D_model.txt"
+    data_file = dir / "synth_data_15D_c1_1expA_125s_v3.csv"
+    parameter_file = dir / "15D_transporter_c1_w_full_priors.json"
     with open(model_file, "r") as f:
-        antimony_string_SS = f.read()
-    rr_models = [te.loada(antimony_string_SS) for _ in range(n_sims)]
-    y_obs= np.genfromtxt(obs_data_file)
+        model_string = f.read()
     with open (parameter_file, 'rb') as fp:
         p_info = json.load(fp)
     p_ref, p_labels, p_bounds = parse_p_info(p_info, near_global_min=False)
+    _, _, p_bounds2 = parse_p_info(p_info, near_global_min=True)
+    rr_model = te.loada(model_string)
 
-    log_like_ref = calc_log_like(p_ref,y_obs,rr_models[0])  # testing
+    ### sampling arguments
+    n_cpus = 2
+    n_walkers = 10
+    n_dim = 15
+    additional_samples = int(1e2)
+    gamma = 0.75 # (0,1) avg. correlation between walkers threshold (low-->more mcmc steps)
+    ess = 0.95 # (0,1) effect sample size (~% overlap) to move to next beta value (low-->less beta stages)
+
+    ### set log likelihood arguments and initial parameter sets
+    y_obs= np.genfromtxt(data_file)
+    p_0 = get_p0(p_bounds, n_walkers) 
+    p_0_2 = get_p0(p_bounds2, n_walkers) 
+    log_like_ref = calc_log_like(p_ref,y_obs,te.loada(model_string))
     print(f"log likelihood reference: {log_like_ref}")
 
-    # create a processing pool
-    p = mp.Pool(processes=n_cpus)
+    actor_count = 2  
+    simulators = [SimulatorActor.remote(model_string, y_obs) for _ in range(actor_count)]
 
-    # perform the simulations
-    log_like_array = np.array(p.starmap(calc_log_like, [(p_ref, y_obs, rr_models[i]) for i in range(n_sims)]))
-    p.close()
-    print(log_like_array)
-    assert(np.isclose(log_like_array.all(),log_like_ref))
+    f = lambda a, b : a.logl.remote(b)
+
+    def calc_log_prior2(*args):
+        '''calculates the log of uniform prior distribution
+        if parameter is in range, the probability is log(1)-->0, else log(0)-->-inf
+        '''
+
+        p = args[0]
+
+        # fix this later - should connect w/ parameter info data file
+        b = 0  # shift parameter priors by 2 (orders of magnitude) to be non-centered
+        lb = np.array([6, -1, -2, -2, 3, -1, -1, 6, -2, -2, -1, 6, -1, 3]) + b  # log10 rate constant prior lower bound + shift
+        ub = np.array([12, 5, 4, 4, 9, 5, 5, 12, 4, 4, 5, 12, 5, 9]) + b  # log10 rate constant prior upper bound + shift
+        sigma_lb = np.log10(5e-14)  # log10 noise sigma prior lower bound 
+        sigma_ub = np.log10(5e-13)  # log10 noise sigma prior upper bound 
+        if ((lb < p[:-1]) & (p[:-1] < ub)).all() and (sigma_lb<p[-1]<sigma_ub):
+            return 0
+        else:
+            return -1e50#-np.inf
+
+    actor_pool = ActorPool(simulators)
+    sampler = pc.Sampler(
+            n_walkers,
+            n_dim,
+            log_likelihood=f, 
+            log_prior=calc_log_prior2,
+            vectorize_likelihood=False,
+            vectorize_prior=False,
+            bounds=p_bounds,
+            random_state=seed,
+            infer_vectorization=False,
+            log_likelihood_args = None,
+            pool=actor_pool
+        ) 
+
+    sampler.log_likelihood = f
+    sampler.run(p_0, ess=ess, gamma=gamma, progress=True)
+    sampler.add_samples(additional_samples)
     
+    results = sampler.results
+    pc.plotting.corner(results)
+    plt.savefig('test.png')
